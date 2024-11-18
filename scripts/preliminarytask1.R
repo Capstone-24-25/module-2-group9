fb75eb261024e99a8aa9c3080483c82807d0d695
Rebecca Chang <102687358+beccs24@users.noreply.github.com>
2024-11-17 21:03:16 -0800

    updated task1 and task2

--- scripts/preliminarytask1.R
+++ scripts/preliminarytask1.R
@@ -1,21 +1,25 @@
 # packages
+require(tidyverse) 
+require(tidytext)
+require(textstem)
+require(rvest)
+require(rsample)
+require(qdapRegex)
+require(stopwords)
+require(tokenizers)
 library(tidyverse)
 library(tidymodels)
 library(modelr)
 library(Matrix)
 library(sparsesvd)
 library(glmnet)
+library(yardstick)
 
 # path to activity files on repo
-# url <- 'https://raw.githubusercontent.com/pstat197/pstat197a/main/materials/activities/data/'
+url <- 'https://raw.githubusercontent.com/pstat197/pstat197a/main/materials/activities/data/'
 
 # load a few functions for the activity
-#source(paste(url, 'projection-functions.R', sep = ''))
-
-# read in data
-#claims <- paste(url, 'claims-multi-tfidf.csv', sep = '') %>%
-#  read_csv()
-
+source(paste(url, 'projection-functions.R', sep = ''))
 
 # can comment entire section out if no changes to preprocessing.R
 source('scripts/preprocessing.R')
@@ -29,7 +33,6 @@ claims_clean <- claims_raw %>%
 
 # export
 save(claims_clean, file = 'data/claims-clean-example.RData')
-
 
 
 ## WITHOUT HEADERS
@@ -72,51 +75,36 @@ nlp_fn <- function(parse_data.out){
                                              '[[:punct:]]')) %>%
     mutate(token.lem = lemmatize_words(token)) %>%
     filter(str_length(token.lem) > 2) %>%
-    count(.id, bclass, token.lem, name = 'n') %>%
+    count(.id, bclass, mclass, token.lem, name = 'n') %>%
     bind_tf_idf(term = token.lem, 
                 document = .id,
                 n = n) %>%
-    pivot_wider(id_cols = c('.id', 'bclass'),
+    pivot_wider(id_cols = c('.id', 'bclass', 'mclass'),
                 names_from = 'token.lem',
                 values_from = 'tf_idf',
                 values_fill = 0)
   return(out)
 }
 
-#turn into tf-idf
-no_headers_tfidf <- claims_clean %>% 
-  unnest_tokens(output = token, 
-                input = text_clean, 
-                token = 'words',
-                stopwords = str_remove_all(stop_words$word, 
-                                           '[[:punct:]]')) %>%
-  mutate(token.lem = lemmatize_words(token)) %>%
-  filter(str_length(token.lem) > 2) %>%
-  count(.id, bclass, mclass, token.lem, name = 'n') %>%
-  bind_tf_idf(term = token.lem, 
-              document = .id,
-              n = n) %>%
-  pivot_wider(id_cols = c('.id', 'bclass', 'mclass'),
-              names_from = 'token.lem',
-              values_from = 'tf_idf',
-              values_fill = 0)
+no_headers_tfidf <- nlp_fn(claims_clean)
 
 
 # partition data
 set.seed(110122)
 partitions1 <- no_headers_tfidf %>% initial_split(prop = 0.8)
 
-# separate DTM from labels
-test_dtm1 <- testing(partitions1) %>%
-  select(-.id, -bclass, -mclass)
-test_labels1 <- testing(partitions1) %>%
-  select(.id, bclass, mclass)
-
-# same, training set
+# separate DTM from labels training set
 train_dtm1 <- training(partitions1) %>%
   select(-.id, -bclass, -mclass)
 train_labels1 <- training(partitions1) %>%
-  select(.id, bclass, mclass)
+  select(.id, bclass, -mclass)
+
+# separate DTM from labels testing set
+test_dtm1 <- testing(partitions1) %>%
+  select(-.id, -bclass, -mclass)
+test_labels1 <- testing(partitions1) %>%
+  select(.id, bclass, -mclass)
+
 
 # find projections based on training data
 proj_out1 <- projection_fn(.dtm = train_dtm1, .prop = 0.7)
@@ -136,7 +124,6 @@ fit1 <- glm(bclass~., data = train1, family = binomial)
 #project test data
 test_dtm_projected1 <- reproject_fn(.dtm = test_dtm1, proj_out1)
 
-#get predictions
 preds1 <- predict(fit1,
                   newdata = as.data.frame(test_dtm_projected1),
                   type = 'response')
@@ -147,6 +134,12 @@ pred_df1 <- test_labels1 %>%
   bind_cols(pred = as.numeric(preds1)) %>%
   mutate(bclass.pred = factor(pred > 0.5, 
                               labels = levels(bclass)))
+
+# Evaluate errors on test set
+class_metrics <- metric_set(sensitivity, 
+                            specificity, 
+                            accuracy,
+                            roc_auc)
 
 # Calculate metrics
 pred_df1 %>% class_metrics(truth = bclass, 
@@ -160,12 +153,10 @@ pred_df1 %>% class_metrics(truth = bclass,
 
 
 
-
-
 # WITH HEADERS
 parse_fn <- function(.html){
   read_html(.html) %>%
-    html_elements('p, h1') %>%
+    html_elements('p, h1, h2, h3, h4, h5, h6') %>%
     html_text2() %>%
     str_c(collapse = ' ') %>%
     rm_url() %>%
@@ -201,11 +192,11 @@ nlp_fn <- function(parse_data.out){
                                              '[[:punct:]]')) %>%
     mutate(token.lem = lemmatize_words(token)) %>%
     filter(str_length(token.lem) > 2) %>%
-    count(.id, bclass, token.lem, name = 'n') %>%
+    count(.id, bclass, mclass, token.lem, name = 'n') %>%
     bind_tf_idf(term = token.lem, 
                 document = .id,
                 n = n) %>%
-    pivot_wider(id_cols = c('.id', 'bclass'),
+    pivot_wider(id_cols = c('.id', 'bclass', 'mclass'),
                 names_from = 'token.lem',
                 values_from = 'tf_idf',
                 values_fill = 0)
@@ -243,16 +234,16 @@ headers_tfidf <- headers_clean %>%
 set.seed(110122)
 partitions <- headers_tfidf %>% initial_split(prop = 0.8)
 
-# separate DTM from labels
-test_dtm <- testing(partitions) %>%
-  select(-.id, -bclass, -mclass)
-test_labels <- testing(partitions) %>%
-  select(.id, bclass, mclass)
-
-# same, training set
+# separate DTM from labels training set
 train_dtm <- training(partitions) %>%
   select(-.id, -bclass, -mclass)
 train_labels <- training(partitions) %>%
+  select(.id, bclass, mclass)
+
+# separate DTM from labels testing set
+test_dtm <- testing(partitions) %>%
+  select(-.id, -bclass, -mclass)
+test_labels <- testing(partitions) %>%
   select(.id, bclass, mclass)
 
 
@@ -298,8 +289,8 @@ class_metrics <- metric_set(sensitivity,
                             roc_auc)
 
 # Calculate metrics
-pred_df %>% class_metrics(truth = bclass, 
+metrics <- pred_df %>% class_metrics(truth = bclass, 
                           estimate = bclass.pred, 
                           pred, 
                           event_level = 'second')
-
+metrics
---
+++ scripts/preliminarytask2.R
@@ -0,0 +1,89 @@
+set.seed(110122)
+
+# Tokenize into bigrams
+headers_bigrams <- claims_clean_headers %>%
+  select(.id, bclass, text_clean) %>%
+  unnest_tokens(output = bigram, 
+                input = text_clean, 
+                token = 'ngrams', 
+                n = 2, 
+                stopwords = str_remove_all(stop_words$word, '[[:punct:]]'))
+
+# Count bigrams and compute TF-IDF
+headers_bigrams_tfidf <- headers_bigrams %>%
+  count(.id, bclass, bigram, name = 'n') %>%
+  bind_tf_idf(term = bigram, 
+              document = .id, 
+              n = n) %>%
+  filter(n>=5) %>% 
+  pivot_wider(id_cols = c(.id, bclass),
+              names_from = bigram,
+              values_from = tf_idf,
+              values_fill = 0)
+
+# Partition data
+partitions_bigrams <- headers_bigrams_tfidf %>% initial_split(prop = 0.8)
+
+train_dtm_bigrams <- training(partitions_bigrams) %>%
+  select(-.id, -bclass)
+train_labels_bigrams <- training(partitions_bigrams) %>%
+  select(.id, bclass)
+
+test_dtm_bigrams <- testing(partitions_bigrams) %>%
+  select(-.id, -bclass)
+test_labels_bigrams <- testing(partitions_bigrams) %>%
+  select(.id, bclass)
+
+# first logistic regression
+
+# PCA projection for bigram data
+train_dtm_bigrams_sparse <- train_dtm_bigrams %>%
+  as.matrix() %>%
+  as('sparseMatrix') 
+svd_out_bigrams <- sparsesvd(train_dtm_bigrams_sparse, rank=173)
+
+#projected data frame
+train_dtm_projected2 <- svd_out_bigrams$u %*% diag(svd_out_bigrams$d)
+
+# assign column names
+colnames(train_dtm_projected2) <- paste0("PC", 1:ncol(train_dtm_projected2))
+
+#regression
+train2 <- train_labels_bigrams %>%
+  transmute(bclass = factor(bclass)) %>%
+  bind_cols(train_dtm_projected2)
+
+
+fit2 <- glm(bclass~., data = train2, family = binomial)
+
+# re-projection
+reproject_fn1 <- function(.dtm, train_projected) {
+  .dtm_sparse <- as(.dtm, "sparseMatrix")
+  test_projected <- as.matrix(.dtm_sparse %*% train_projected$v %*% diag(1 / train_projected$d))
+  colnames(test_projected) <- paste0("PC", 1:ncol(test_projected))
+  return(test_projected)
+}
+
+
+#project test data
+test_dtm_projected2 <- reproject_fn1(.dtm = test_dtm_bigrams, svd_out_bigrams)
+
+#get predictions
+preds2 <- predict(fit2,
+                  newdata = as.data.frame(test_dtm_projected2),
+                  type = 'response')
+
+#test-labels with predictions
+pred_df2 <- test_labels_bigrams %>%
+  transmute(bclass = factor(bclass)) %>%
+  bind_cols(pred = as.numeric(preds2)) %>%
+  mutate(bclass.pred = factor(pred > 0.5, 
+                              labels = levels(bclass)))
+
+# Calculate metrics
+metrics2 <- pred_df2 %>% class_metrics(truth = bclass, 
+                           estimate = bclass.pred, 
+                           pred, 
+                           event_level = 'second')
+
+metrics2
